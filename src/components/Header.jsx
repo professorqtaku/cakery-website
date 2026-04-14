a4332533235019873719552488d4ea228b413876
Qiling Tan <72065263+professorqtaku@users.noreply.github.com>
2026-04-14 23:26:12 +0200

    add styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,16 +1,22 @@
 import { NavLink } from "react-router-dom";
+import logo from "../assets/logo.png";
 
 function Header() {
     return (
         <header>
-            <h1>Qwerty Cakery</h1>
-
-            <nav>
-                <NavLink to="/" end>Home</NavLink>
-                <NavLink to="/about">About</NavLink>
-                <NavLink to="/products">Products</NavLink>
-                <NavLink to="/contact">Contact</NavLink>
-            </nav>
+            <div class="container">
+                <button class="bg-primary text-white px-4 py-2 rounded-md">Say Hello</button>
+                <h1>Munamii Cakery</h1>
+                <img src={logo} alt="Munamii Cakery Logo" />
+            </div>
+            <div>
+                <nav>
+                    <NavLink to="/" end>Home</NavLink>
+                    <NavLink to="/about">About</NavLink>
+                    <NavLink to="/products">Products</NavLink>
+                    <NavLink to="/contact">Contact</NavLink>
+                </nav>
+            </div>
         </header>
     );
 }
