55d091a1896319a52021760bc14130ff8bf7d973
Qiling Tan <72065263+professorqtaku@users.noreply.github.com>
2026-04-15 14:05:54 +0200

    style button

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,7 +1,7 @@
 import { ButtonType } from './buttonTypes';
 
 const Button = ({ onClick, buttonType = ButtonType.PRIMARY, children }) => {
-    const baseClasses = 'px-4 py-2 rounded-full text-white';
+    const baseClasses = 'px-4 py-2 rounded-full text-white hover:opacity-90 transition-opacity duration-300 font-body hover:cursor-pointer'
     const typeClasses = buttonType === ButtonType.PRIMARY ? 'bg-primary' : 'bg-secondary';
 
     return (
