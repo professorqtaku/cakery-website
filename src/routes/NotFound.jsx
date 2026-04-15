aca4a797c0d1fafac267ba16787bf59a18906012
Qiling Tan <72065263+professorqtaku@users.noreply.github.com>
2026-04-15 14:05:41 +0200

    404 not found

--- src/routes/NotFound.jsx
+++ src/routes/NotFound.jsx
@@ -1,13 +1,18 @@
-import { Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
+import Button from "../components/Button";
 
 function NotFound() {
-    return (
-        <section>
-            <h2>404 – Page Not Found</h2>
-            <p>The page you are looking for does not exist.</p>
+    const navigate = useNavigate();
 
-            <Link to="/">Go back to Home</Link>
-        </section>
+    return (
+        <div className="pt-20 pb-20 px-6 max-w-7xl mx-auto text-center">
+            <section>
+                <h1 className="text-4xl mb-4">404 – Page Not Found</h1>
+                <p className="mb-4">The page you are looking for does not exist.</p>
+
+                <Button onClick={() => navigate('/')}>Go back to Home</Button>
+            </section>
+        </div>
     );
 }
 
